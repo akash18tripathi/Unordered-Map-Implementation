41e363649febc6463e0d73d9334771075cace07e
akash18tripathi <akash18tripathi@gmail.com>
2023-07-02 20:51:48 +0530

    Adding Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,104 @@
 # Unordered-Map-Implementation
+
+This repository contains a C++ implementation of an unordered map that demonstrates how hashing works and the importance of a hash function. The unordered map is generic and can handle different data types such as int, float, char, double, and string. 
+
+### Functions to Implement
+
+The following functions are implemented in the code:
+
+1. `insert(key, value)`: Inserts a key-value pair into the unordered map. If the key already exists, the corresponding value is updated.
+
+2. `erase(key)`: Erases the key from the unordered map if it exists; otherwise, no action is taken.
+
+3. `find(key)`: Returns true if the key exists in the unordered map; otherwise, returns false.
+
+4. `map[key]`: Returns the value associated with the key in the unordered map. If the key does not exist, the default value of the corresponding data type is returned.
+
+### Input Format
+
+The input should follow the specified format:
+
+- The first line should contain a single integer `q` denoting the number of queries.
+
+- Each subsequent line corresponds to a query and consists of two lines:
+
+    - The first line of a query should contain a single integer representing the type of operation:
+        - `1` for insert
+        - `2` for erase
+        - `3` for find
+        - `4` for map
+        
+    - The second line of a query contains the inputs based on the type of operation:
+        - For type `1` (insert): two strings denoting the key and value to be inserted.
+        - For type `2` (erase): one string denoting the key to be erased.
+        - For type `3` (find): one string denoting the key to be found.
+        - For type `4` (map): one string denoting the key on which the operation must be applied.
+        
+### Output Format
+
+The output format depends on the type of operation:
+
+- For type `1` (insert) and type `2` (erase), no output is printed.
+
+- For type `3` (find):
+    - If the key exists in the unordered map, `1` is printed.
+    - If the key does not exist, `0` is printed.
+    
+- For type `4` (map), the value associated with the key is printed.
+
+### File Structure
+
+This repository contains a single C++ file named `unordered_map.cpp`. You can find the complete implementation of the unordered map with the required functions in this file.
+
+Please refer to the code file for detailed implementation and usage instructions.
+
+
+## Problem: Counting Distinct Elements in Subarrays
+
+Using the unordered map implemented in this repository, we are solving the following question for testing the accuracy for implementation:
+
+### Problem Description
+
+You are given an array of size N. Your task is to count the number of distinct elements in every subarray of size K.
+
+#### Input Format
+
+The first line contains two integers N and K, where N denotes the size of the array and K denotes the size of the subarray.
+
+The second line contains N space-separated integers denoting the values of the individual elements in the array.
+
+#### Output Format
+
+A single line that contains N - K + 1 elements denoting the number of distinct elements in each individual subarray.
+
+#### Constraints
+
+1 <= N <= 10^4
+
+1 <= K <= N
+
+#### Sample Input
+```shell
+7 4
+1 2 1 3 4 2 3
+```
+#### Sample Output
+```shell
+3 4 4 3
+```
+
+
+#### Explanation
+
+- The first window is {1, 2, 1, 3}, and the count of distinct numbers is 3.
+
+- The second window is {2, 1, 3, 4}, and the count of distinct numbers is 4.
+
+- The third window is {1, 3, 4, 2}, and the count of distinct numbers is 4.
+
+- The fourth window is {3, 4, 2, 3}, and the count of distinct numbers is 3.
+
+
+## Contribution
+
+Please feel free for suggestions/improvements in the above implementation.
---
+++ unordered_map.cpp
@@ -0,0 +1,191 @@
+#include <iostream>
+#include <sstream> 
+
+#define bucket_size 100003
+
+using namespace std;
+
+
+template<class A,class B> 
+class Unordered_Map{
+
+    public:
+    class Node{
+        public:
+
+        A key;
+        B val;
+        Node* right;
+
+        Node(){
+
+        }
+        
+        Node(A k,B v){
+            this->key=k;
+            this->val=v;
+            this->right=NULL;
+        }
+    };
+
+    Node** arr;
+
+    Unordered_Map(){
+        arr = new Node*[bucket_size];
+    }
+
+    int hashF(int key){
+        return key%bucket_size;
+    }
+    int hashF(float key){
+        int temp = (int) key;
+        return temp%bucket_size;
+    }
+    int hashF(double key){
+        int temp = (int) key;
+        return temp%bucket_size;
+    }
+    int hashF(char key){
+        int temp = (int) key;
+        return temp%bucket_size;
+    }
+    int hashF(string key){
+
+        string input = key;
+        int hashVal=0;
+        int tempVar=1;
+        for(int i=0;i<input.length();i++){
+            int asci = int(input[i]);
+            hashVal = (hashVal+ (asci*tempVar)%bucket_size)%bucket_size;
+            tempVar = (tempVar*13)%bucket_size;
+        }
+        return hashVal%bucket_size;
+    }
+
+
+    void insert(A k,B v){
+        int hashVal = this->hashF(k);
+        if(arr[hashVal]==NULL){
+            Node* n =new Node(k,v);
+            arr[hashVal]=n;
+        }else{
+            Node* temp = arr[hashVal];
+            while(temp->right!=NULL){
+                if(temp->key==k){
+                    temp->val=v;
+                    return;
+                }
+                temp = temp->right;
+            }
+            if(temp->key==k){
+                temp->val=v;
+                return;
+            }
+            Node* n = new Node(k,v);
+            temp->right=n;
+        }
+    }
+
+    void erase(A k){
+        int hashVal = this->hashF(k);
+        if(arr[hashVal]==NULL){
+            return;
+        }
+        if(arr[hashVal]->key==k){
+            arr[hashVal] = arr[hashVal]->right;
+            return;
+        }
+        Node* temp = arr[hashVal];
+        while(temp->right!=NULL && temp->right->key!=k){
+            temp=temp->right;
+        }
+        if(temp->right==NULL){
+            return;
+        }
+        Node* n = temp->right;
+        temp->right = n->right;
+        n->right=NULL;
+        free(n);
+    }
+
+    bool find(A k){
+        int hashVal = this->hashF(k);
+        if(arr[hashVal]==NULL){
+            return false;
+        }
+        Node* temp = arr[hashVal];
+        while(temp!=NULL){
+            if(temp->key==k){
+                return true;
+            }
+            temp=temp->right;
+        }
+        return false;
+        
+    }
+
+    B operator[](A k){
+        int hashVal=this->hashF(k);
+        Node* p = arr[hashVal];
+        while(p!=NULL){
+            if(p->key==k) return p->val;
+            p=p->right;  
+        }
+        B df = B();
+        return df;
+    }
+
+
+};
+
+
+
+
+int main(){
+    
+    int n,k;
+    cin >> n >> k;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    Unordered_Map<int,int> mp;
+    int distinctElements=0;
+
+    for(int i=0;i<k;i++){
+        if(mp.find(arr[i])){
+            mp.insert(arr[i],mp[arr[i]]+1);
+        }else{
+            distinctElements++;
+            mp.insert(arr[i],1);
+        }
+    }
+    int ans[n-k+1];
+    int window=0;
+    ans[window]=distinctElements;
+    int i=1,j=k;
+    while(j<n){
+        window++;
+        mp.insert(arr[i-1],mp[arr[i-1]]-1);
+        if(mp[arr[i-1]]==0){
+            distinctElements--;
+            mp.erase(arr[i-1]);
+        }
+        if(mp.find(arr[j])){
+            mp.insert(arr[j],mp[arr[j]]+1);
+        }else{
+            mp.insert(arr[j],1);
+            distinctElements++;
+        }
+        ans[window]=distinctElements;
+        i++;
+        j++;
+    }
+
+    for(int i=0;i<n-k+1;i++){
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
